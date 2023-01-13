91f73740e953e448f52a7e914b079c64a8172ef6
k3yof <mvicrev288@gmail.com>
2023-01-13 13:54:12 +0100

    a

--- tema7(obetosyclases)/camiones/camion.java
+++ tema7(obetosyclases)/camiones/camion.java
@@ -5,21 +5,32 @@ import java.util.Scanner;
 
 class Punto2d
 {
-   int x=0;
-   int y=0;
+   double x;
+   double y;
 
     //Constructor
     Punto2d()//Constructor por defecto
     {
-      
+      x=0;
+      y=0;
     }
     //Comportamiento
 
-    int  getPosicion()
+    double  getPosicionX()
     {
-        
+        return x;
     }
-}
+    double  getPosicionY()
+    {
+        return y;
+    }
+    double  getUbicacion()
+    {
+        double c = x+y;
+
+        return c;
+    }
+
 
     
     int[] insertar(int resul[], int elem)
@@ -32,12 +43,9 @@ class Punto2d
         return resul;
     }  
 
-    int [][] setPosicion(int fila,int cols)
+    void setPosicion(int fila,int cols)
     {
-       // secuencia = Arrays.copyOf(resp, num);
-        posicion=insertar(posicion, fila);
        
-        return posicion;
 
     }
 
@@ -49,52 +57,18 @@ public class camion
     public static void main(String[] args) 
     {
        Scanner sc = new Scanner(System.in);       
-       int caras=6;
-       Punto2d dado6caras;
+  
+       Punto2d camion1;
        
        //s1 = new Semaforo1();
 
        //Constructoor por defecto
-       dado6caras = new Punto2d(caras);
+       camion1 = new Punto2d();
+
+       camion1.getUbicacion();
 
 
-      
-       
 
-       System.out.println(dado6caras.tirar());
-       dado6caras.cont++;
-
-       System.out.println("--------------------------------");
-       System.out.println(Arrays.toString(dado6caras.secuencia));
-       System.out.println("--------------------------------");
-       sc.close();
-       //crear un vector de dados
-       //con un bucle hacer unos cuantos lanzamientos
-
-       int [] vectorEntero;
-
-       vectorEntero = new int [5];
-
-       Punto2d []vectorDados;
-       vectorDados = new Punto2d[5];//Reservamos memoria para referencias
-
-       //dos de 6
-       //1 de 12
-       //2 de 400
-       for(int i=0;i<2;i++)
-       {
-       vectorDados[i] = new Punto2d(6) ;
-       System.out.println(vectorDados[i].getCartas());
-       }
-
-       vectorDados[3] = new Punto2d(12) ;
-       System.out.println(vectorDados[3].getCartas());
-
-       for(int i=3;i<5;i++)
-       {
-            vectorDados[i] = new Punto2d(400) ;
-            System.out.println(vectorDados[i].getCartas());
-       }
 
 
 
