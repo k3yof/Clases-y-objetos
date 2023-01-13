d5987debee811212507a6f682c92af6f1ad2091b
k3yof <mvicrev288@gmail.com>
2023-01-13 13:50:04 +0100

    a

--- tema7(obetosyclases)/camiones/camion.java
+++ tema7(obetosyclases)/camiones/camion.java
@@ -5,107 +5,23 @@ import java.util.Scanner;
 
 class Punto2d
 {
-    int caras;//El numero hasta qu llegara el dado por ejemplo si las caras son 6 habra hasta el numero 6
-    int secuencia[];//Para ir guardando la secuencia del dado
-    int resp[];
-    int cont=0;
+   int x=0;
+   int y=0;
 
     //Constructor
     Punto2d()//Constructor por defecto
     {
-        caras = 6;
-        secuencia = new int [cont];
-        resp = new int [cont];
+      
     }
-    Punto2d(int caras)//Constructor por defecto
-    {
-        if (caras < 2)
-        {
-            this.caras = 2;
-            secuencia = new int [cont];
-            resp = new int [cont];
-        }
-        else
-        {
-            this.caras = caras;
-            secuencia = new int [cont];
-            resp = new int [cont];
-        }
-        
-    }<z
     //Comportamiento
 
-    int getCont()
+    int  getPosicion()
     {
-        return cont;
-    }
-
-    int getCartas()
-    {
-        return caras;
-    }
-
-    int[] insertar(int resul[], int elem)
-    //inserta un nuevo elemento en el array pasado como argumento
-    //SIN respetar orden
-    {
-        resul = Arrays.copyOf(resul, resul.length+1);
-        resul[resul.length-1] = elem;
-
-        return resul;
-    }  
-
-    int tirar()
-    {
-        int num = (int) (Math.random()*caras+1);
         
-       // secuencia = Arrays.copyOf(resp, num);
-        secuencia=insertar(secuencia, num);
-        cont++;
-        return num;
-
     }
-
 }
-class DadoAlea1
-{
-    int caras;//El numero hasta qu llegara el dado por ejemplo si las caras son 6 habra hasta el numero 6
-    int secuencia[];//Para ir guardando la secuencia del dado
-    int resp[];
-    int cont=0;
-    //Constructor
-    DadoAlea1()//Constructor por defecto
-    {
-        caras = 6;
-        secuencia = new int [cont];
-        resp = new int [cont];
-    }
-    DadoAlea1(int caras)//Constructor por defecto
-    {
-        if (caras < 2)
-        {
-            this.caras = 2;
-            secuencia = new int [cont];
-            resp = new int [cont];
-        }
-        else
-        {
-            this.caras = caras;
-            secuencia = new int [cont];
-            resp = new int [cont];
-        }
-        
-    }
-    //Comportamiento
-    int getCont()
-    {
-        return cont;
-    }
-    int getCartas()
-    {
-        return caras;
-    }
 
+    
     int[] insertar(int resul[], int elem)
     //inserta un nuevo elemento en el array pasado como argumento
     //SIN respetar orden
@@ -116,16 +32,12 @@ class DadoAlea1
         return resul;
     }  
 
-    int tirar()
+    int [][] setPosicion(int fila,int cols)
     {
-        int num = (int) (Math.random()*caras+1);
-        
-        
-        //secuencia = Arrays.copyOf(resp, num);
-        secuencia=insertar(secuencia, num);
-        cont++;
-        return num;
-
+       // secuencia = Arrays.copyOf(resp, num);
+        posicion=insertar(posicion, fila);
+       
+        return posicion;
 
     }
 
@@ -138,29 +50,23 @@ public class camion
     {
        Scanner sc = new Scanner(System.in);       
        int caras=6;
-       DadoAlea dado6caras;
-       DadoAlea1 dado12caras;
+       Punto2d dado6caras;
+       
        //s1 = new Semaforo1();
 
        //Constructoor por defecto
-       dado6caras = new DadoAlea(caras);
-       dado12caras = new DadoAlea1(caras);
+       dado6caras = new Punto2d(caras);
 
-       System.out.print("Cuantas caras quieres q tenga el dado: ");
-       caras=sc.nextInt();
+
+      
+       
 
        System.out.println(dado6caras.tirar());
        dado6caras.cont++;
-       System.out.println(dado12caras.tirar());
-       dado12caras.cont++;
-       System.out.println(dado12caras.tirar());
-       dado12caras.cont++;
-       System.out.println(dado12caras.tirar());
-       dado12caras.cont++;
+
        System.out.println("--------------------------------");
        System.out.println(Arrays.toString(dado6caras.secuencia));
        System.out.println("--------------------------------");
-       System.out.println(Arrays.toString(dado12caras.secuencia));
        sc.close();
        //crear un vector de dados
        //con un bucle hacer unos cuantos lanzamientos
@@ -169,24 +75,24 @@ public class camion
 
        vectorEntero = new int [5];
 
-       DadoAlea []vectorDados;
-       vectorDados = new DadoAlea[5];//Reservamos memoria para referencias
+       Punto2d []vectorDados;
+       vectorDados = new Punto2d[5];//Reservamos memoria para referencias
 
        //dos de 6
        //1 de 12
        //2 de 400
        for(int i=0;i<2;i++)
        {
-       vectorDados[i] = new DadoAlea(6) ;
+       vectorDados[i] = new Punto2d(6) ;
        System.out.println(vectorDados[i].getCartas());
        }
 
-       vectorDados[3] = new DadoAlea(12) ;
+       vectorDados[3] = new Punto2d(12) ;
        System.out.println(vectorDados[3].getCartas());
 
        for(int i=3;i<5;i++)
        {
-            vectorDados[i] = new DadoAlea(400) ;
+            vectorDados[i] = new Punto2d(400) ;
             System.out.println(vectorDados[i].getCartas());
        }
 
