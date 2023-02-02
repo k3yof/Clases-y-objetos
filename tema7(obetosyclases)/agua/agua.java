c3e23dcf578745eba17e2a8a5be3676e0a956e77
k3yof <mvicrev288@gmail.com>
2023-02-02 08:48:55 +0100

    a

---
+++ tema7(obetosyclases)/agua/agua.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class calendar
+{
+    private char [] abecedarioMayusculas= {'A','B','C','D','E','F','G','H','I',' ','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    private char [] abecedarioMinusculas= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+   
+    
+    private int convertidor()
+    {
+
+    }
+    public String cifradorCesar(int cantidadDerecha,String nombreReal)
+    {
+        String mensajeCifrado="";
+        int cont =0;
+        int longitud;
+        longitud = nombreReal.length;
+
+        if (cont <= longitud)
+        {
+            nombreReal[cont];    
+        }
+       nombreReal[0]= abecedarioMayusculas[cantidadDerecha];
+        
+
+        return mensajeCifrado;
+    }
+
+}
+
+class agua
+{
+    public static void main(String [] args)
+    {
+    Scanner sc = new Scanner(System.in);
+    } 
+}
