d25ea47c2d332262a362140565651dca54a50d7d
k3yof <mvicrev288@gmail.com>
2023-02-09 13:07:42 +0100

    a

---
+++ tema7(obetosyclases)/Abstracto/m.java
@@ -0,0 +1,29 @@
+package Abstracto;
+abstract class A
+{
+    abstract void metodo2();
+}
+class B extends A
+{
+    void metodo2()
+    {
+
+    }
+}
+class C extends B
+{
+    void metodo2()
+    {
+        
+    }
+}
+public class m
+{
+
+    public static void main(String[] args) 
+    {
+       
+
+    }
+     
+}
