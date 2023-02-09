7240600a875dfd297c1ee74e10e7f0dfaa1be96e
k3yof <mvicrev288@gmail.com>
2023-02-09 13:10:09 +0100

    a

--- tema7(obetosyclases)/Abstracto/m.java
+++ tema7(obetosyclases)/Abstracto/m.java
@@ -7,14 +7,14 @@ class B extends A
 {
     void metodo2()
     {
-
+        System.out.print("a");
     }
 }
 class C extends B
 {
     void metodo2()
     {
-        
+        System.out.print("b");
     }
 }
 public class m
@@ -23,7 +23,9 @@ public class m
     public static void main(String[] args) 
     {
        
+        C c = new C();
 
+        c.metodo2();
     }
      
 }
