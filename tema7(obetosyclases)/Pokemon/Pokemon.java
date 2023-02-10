221edf0795d97c8090c241e78aefb68fb06a621c
k3yof <mvicrev288@gmail.com>
2023-02-10 10:48:17 +0100

    a

--- tema7(obetosyclases)/Pokemon/Pokemon.java
+++ tema7(obetosyclases)/Pokemon/Pokemon.java
@@ -6,6 +6,7 @@ abstract class Pokemom
     protected String entrenador;
     protected int experiencia;
     protected int salud;
+    protected int dañoProducido=0;
 
     abstract void mostrar();
 }
@@ -21,22 +22,98 @@ class Pichu extends Pokemom
         public void mostrar()
         {
             System.out.println("----------------------");
-            System.out.println(" Nombre: "+this.nombre+"   ");
+            System.out.println(" Nombre: "+nombre+"   ");
             System.out.println(" Entrenador: "+this.entrenador+"   ");
             System.out.println(" Experiencia: "+this.experiencia+"    ");
             System.out.println(" Salud: "+this.salud+"          ");
             System.out.println("----------------------");
         }
-        public int ataqueRapido()
+        public int ataqueRapido1()
         {
-            int dañoProducido=0;
+            int dañoJugada=0;
             int d = (int) (Math.random()*10)+1;
             if (d ==1 || d ==2 || d ==3 || d ==4 || d ==5)
             {
-                dañoProducido = 
+                dañoJugada = 10;
+                dañoProducido = dañoProducido+10;
             }
-            return dañoProducido;
+            else
+            {
+                dañoJugada=20;
+                dañoProducido = dañoProducido+20;
+            }
+            return dañoJugada;
         }
+
+        public void ataqueRapido()
+        {
+            
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+            else
+            {
+                System.out.println("Ataque rapido ha hecho : "+ataqueRapido1());
+                System.out.println("Daño : "+this.dañoProducido);
+                if (this.experiencia >= 50)
+                {
+                    System.out.println("Ha Mejorado!!!!");
+                } 
+            }
+        }
+        public int bolaRayo1()
+        {
+            int dañoJugada=0;
+            //int d = (int)(Math.random()*(20-10+1)+10);
+            dañoJugada = 100;
+            this.dañoProducido = dañoProducido+dañoJugada;
+
+            return dañoJugada;
+        }
+
+        public void bolaRayo()
+        {
+            
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+            else
+            {
+                System.out.println("Bola rayo ha hecho : "+ bolaRayo1());
+                System.out.println("Daño : "+this.dañoProducido);
+                if (this.experiencia >= 50)
+                {
+                    System.out.println("Ha Mejorado!!!!");
+                } 
+            }
+        }
+
+        public void subirExp()
+        {
+            if (this.experiencia >= 50)
+            {
+               
+            } 
+
+            else
+            {
+                this.experiencia = this.experiencia+15;
+                System.out.println(this.experiencia);
+                this.dañoProducido =0;
+            }
+        }
+        protected Pikachu mejora()
+        {
+            this.dañoProducido = 0;
+            Pikachu pokemo2 = new Pikachu("Pikachu", this.entrenador,this.experiencia, this.salud+25);
+           // System.out.println("ha mejorado!!!!");
+            return pokemo2;
+
+
+        }
+        
         
 }
 
@@ -44,9 +121,94 @@ class Pikachu extends Pichu
 {
         Pikachu( String nombre, String entrenador,int experiencia,int salud)
         {
-            super(nombre, entrenador, experiencia, salud);
+           super(nombre, entrenador, experiencia, salud);
+           
         }
-        
+        public int ataqueRapido1()
+        {
+            int dañoJugada=0;
+            int d = (int) (Math.random()*10)+1;
+            if (d ==1 || d ==2 || d ==3 || d ==4 || d ==5)
+            {
+                dañoJugada = 15;
+                dañoProducido = dañoProducido+15;
+            }
+            else
+            {
+                dañoJugada=25;
+                dañoProducido = dañoProducido+25;
+            }
+            return dañoJugada;
+        }
+
+        public void ataqueRapido()
+        {
+           
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+            else
+            {
+                System.out.println("Ataque rapido ha hecho : "+ataqueRapido1());
+                System.out.println("Daño : "+this.dañoProducido);
+                if (this.experiencia >= 100)
+                {
+                    System.out.println("Ha Mejorado!!!!");
+                } 
+            }
+        }
+        public int bolaVoltio1()
+        {
+            int dañoJugada=0;
+            //int d = (int)(Math.random()*(50-10+1)+10);
+            dañoJugada = 100;
+            dañoProducido = dañoProducido+dañoJugada;
+
+            return dañoJugada;
+        }
+
+        public void bolaVoltio()
+        {
+            
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+            else
+            {
+                System.out.println("Bola voltio ha hecho : "+bolaVoltio1());
+                System.out.println("Daño : "+this.dañoProducido);
+                if (experiencia >= 100)
+                {
+                    System.out.println("Ha Mejorado!!!!");
+                } 
+            }
+        }
+        public void subirExp()
+        {
+          
+            if (this.experiencia >= 100)
+            {
+                
+            } 
+
+            else
+            {
+                this.experiencia = this.experiencia+15;
+                System.out.println(this.experiencia);
+                this.dañoProducido =0;
+            }
+        }
+        protected Raichu mejora()
+        {
+            this.dañoProducido = 0;
+            Raichu pokemo3 = new Raichu("Raichu", this.entrenador,this.experiencia, this.salud+25);
+            //System.out.println("ha mejorado!!!!");
+            return pokemo3;
+
+
+        }
 }
    
 class Raichu extends Pikachu
@@ -55,6 +217,59 @@ class Raichu extends Pikachu
         {
             super(nombre, entrenador, experiencia, salud);
         }
+        public int ataqueRapido1()
+        {
+            int dañoJugada=0;
+            int d = (int) (Math.random()*10)+1;
+            if (d ==1 || d ==2 || d ==3 || d ==4 || d ==5)
+            {
+                dañoJugada = 30;
+                dañoProducido = dañoProducido+30;
+            }
+            else
+            {
+                dañoJugada=40;
+                dañoProducido = dañoProducido+40;
+            }
+            return dañoJugada;
+        }
+
+        public void ataqueRapido()
+        {
+            System.out.println("Ataque rapido ha hecho : "+ataqueRapido1());
+            System.out.println("Daño : "+this.dañoProducido);
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+        }
+        public int estallidoTrueno1()
+        {
+            int dañoJugada=0;
+            //int d = (int)(Math.random()*(90-10+1)+10);
+            dañoJugada = 100;
+            dañoProducido = dañoProducido+dañoJugada;
+
+            return dañoJugada;
+        }
+
+        public void estallidoTrueno()
+        {
+            System.out.println("Bola voltio ha hecho : "+estallidoTrueno1());
+            System.out.println("Daño : "+this.dañoProducido);
+            if (dañoProducido >=100)
+            {
+                this.subirExp();
+            }
+        }
+        public void subirExp()
+        {
+            this.experiencia = this.experiencia+15;
+            System.out.println(this.experiencia);
+            this.dañoProducido =0;
+            
+        }
+      
         
 }
 
@@ -65,16 +280,60 @@ public class Pokemon
 
     public static void main(String[] args) 
     {
-       Pikachu a = new Pikachu("b", "manu", 42, 50);
+       Pichu a = new Pichu("Pichu", "manu", 0, 50);
 
        a.mostrar();
+    
 
-       int d =0;
-       while (d!=10)
-       {
-        d = (int) (Math.random()*10)+1;
-        System.out.println(d);
-       }
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+       a.bolaRayo();
+
+     
+   
+
+       Pikachu z = a.mejora();
+       z.mostrar();
+      
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+       z.bolaVoltio();
+      
+
+
+       Raichu ak = z.mejora();
+
+       ak.mostrar();
+        
+       ak.estallidoTrueno();
+       ak.estallidoTrueno();
+       ak.estallidoTrueno();
+       ak.estallidoTrueno();
+       ak.estallidoTrueno();
+       ak.estallidoTrueno();
+
+    
        
     }
      
